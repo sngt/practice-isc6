e2ca948a6884e59a737d2767c4beaf485d55db5f
toyoshima_shingo <goradd@gmail.com>
2017-10-08 13:22:42 +0900

    ベンチ用スクリプト調整。

--- bench.sh
+++ bench.sh
@@ -2,6 +2,18 @@ echo -n '' | tee /var/log/nginx/access.log
 echo -n '' | tee /var/log/nginx/error.log
 
 cd /home/isucon/isucon6q/
-./isucon6q-bench
+./isucon6q-bench | tee /tmp/bench_result
 
-cat /var/log/nginx/access.log | awk '{res_time_sum[$3" "$4] += $7;counts[$3" "$4]++}END{for (i in res_time_sum) {print i"\t"res_time_sum[i]}"\t"counts[i]}' | sort -n -k 3
+cat /var/log/nginx/access.log | awk '{
+    gsub(/(\?[^\?]+)$/, "", $4);
+    gsub(/\/keyword\/([^\/]+)$/, "/keyword/(.+)", $4);
+    req = $3" "$4;
+    res_time_sum[req] += $7;
+    counts[req]++;
+} END {
+    for (req in res_time_sum) {
+        print req"\t"res_time_sum[req]"\t"counts[req];
+    }
+}' | sort -n -k 3
+
+cat /tmp/bench_result | /home/isucon/webapp/php/update_keyword.js
